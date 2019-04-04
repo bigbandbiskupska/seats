6822a64fdac8017dbf18b91d18c6d67a5ed95cdc
Krystof Tulinger <k.tulinger@seznam.cz>
2019-04-04 10:05:29 +0200

    fixing travis build

--- tests/helpers/RequestFactory.php
+++ tests/helpers/RequestFactory.php
@@ -5,20 +5,32 @@ namespace App\Tests;
 use Nette\Http\Request;
 use Nette\Http\RequestFactory as NetteRequestFactory;
 
-class RequestFactory extends NetteRequestFactory {
-
+class RequestFactory extends NetteRequestFactory
+{
     private $myRawBodyCallback;
 
-    public function setRawBodyCallback(callable $callback) {
+    public function setRawBodyCallback(callable $callback)
+    {
         $this->myRawBodyCallback = $callback;
     }
 
-    public function createHttpRequest() {
+    public function createHttpRequest()
+    {
         $r = parent::createHttpRequest();
         return new Request(
-                $r->getUrl(), null, $r->getPost(), $r->getFiles(), $r->getCookies(), $r->getHeaders(), $r->getMethod(), $r->getRemoteAddress(), $r->getRemoteHost(), $this->myRawBodyCallback ? $this->myRawBodyCallback : function() use ($r) {
-            return $r->getRawBody();
-        });
+            $r->getUrl(),
+            null,
+            $r->getPost(),
+            $r->getFiles(),
+            $r->getCookies(),
+            $r->getHeaders(),
+            $r->getMethod(),
+            $r->getRemoteAddress(),
+            $r->getRemoteHost(),
+            $this->myRawBodyCallback ? $this->myRawBodyCallback : function () use ($r) {
+                return $r->getRawBody();
+            }
+        );
     }
 
 }
